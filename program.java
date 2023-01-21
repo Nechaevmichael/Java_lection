f81d0ce42d18dbd845503c3757cb11c0f01aaaec
NechaevMichael <nechaevmichael@mail.ru>
2023-01-21 22:50:47 +0500

    Lecton_1

--- program.java
+++ program.java
@@ -1,7 +1,8 @@
 /**
  * program
  */
-public class program {
+import java.util.Scanner;
+ public class program {
     public static void main(String[] args) {
         var msg = "qwerty";
         System.out.println(getType(msg)); // мои комментарии будут здесь
@@ -20,6 +21,38 @@ public class program {
         // a = a << 1
         System.out.println(b << 1);
 
+        // Массивы одномерные
+        int[] arr = new int[10];
+        arr[3] = 13;
+        System.out.println(arr[3]);
+
+        int[] new_arr = new int[] {1, 2, 3, 5};
+        System.out.println(new_arr.length);
+
+        // Многомерные массивы
+        int[] matrix[] = new int[3][5];
+        for (int[] line: matrix) {
+            for (int item: line) {
+                System.out.printf("%d ", item);
+            }
+            System.out.println();
+        }
+
+        int[][] arr1 = new int[3][5];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                System.out.printf("%d ", arr1[i][j]);
+            }
+            System.out.println();
+        }
+        
+        // Ввод данных
+
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("name: ");
+        String name = iScanner.nextLine();
+        System.out.printf("Привет, %s", name);
+        
     }
 
     static String getType(Object o) {
