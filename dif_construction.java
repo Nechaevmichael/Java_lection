043d11ff65d63e9c1b122a7d5bc6e21dafb7cfa4
NechaevMichael <nechaevmichael@mail.ru>
2023-01-22 10:43:21 +0500

    add file dif

---
+++ dif_construction.java
@@ -0,0 +1,15 @@
+public class dif_construction {
+    public static void main(String[] args) { // нахождение максимума
+        int a = 4;
+        int b = 2;
+        int c;
+        if (a > b) {
+            c = a;
+        }
+        else {
+            c = b;
+        }
+        System.out.println(c);
+    }
+    
+}
